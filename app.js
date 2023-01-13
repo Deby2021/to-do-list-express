54fbe67fea99d7dd3186fb05aa37e824b24aa099
Deby2021 <dcc.cabrera1986@gmail.com>
2023-01-13 19:23:45 -0300

    json com express

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
 //root do nosso projeto
 
 const express = require("express");
+const app = express();
+//const port = 3000
+
+app.get("/", (req, res) => {
+  res.send("<h1>Minha lista de Tarefas</h1> :|");
+});
+
+//devolvendo json através do express
+app.get("/json", (req, res) => {
+  res.json({ title: "Tarefa X", done: true });
+});
+
+//app.listen(port, () => {
+//console.log(`Example app listening on port ${port}`);
+//});
+
+app.listen(3000, () => {
+  console.log("Servidor foi iniciado");
+});
