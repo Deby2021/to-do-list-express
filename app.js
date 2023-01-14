4fc67332a1008ab4f8099a0a1879cd518389ec12
Deby2021 <dcc.cabrera1986@gmail.com>
2023-01-14 15:13:25 -0300

    Monstrando JSON com express e criando Middleware

--- app.js
+++ app.js
@@ -4,12 +4,26 @@ const express = require("express");
 const app = express();
 //const port = 3000
 
+//quando precisar passar json pelo express
+app.use(express.json());
+
+//midleware
+const log = (req, res, next) => {
+  console.log(req.body);
+  console.log(`Data: ${Date.now()}`);
+  next();
+};
+
+app.use(log);
+
 app.get("/", (req, res) => {
   res.send("<h1>Minha lista de Tarefas</h1> :|");
 });
 
 //devolvendo json através do express
 app.get("/json", (req, res) => {
+  //monstrando o json através do express
+  //console.log(req.body); já tem
   res.json({ title: "Tarefa X", done: true });
 });
 
